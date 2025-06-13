2461bdc93c46ae452797d112fda758645c03a7d0
WenMinZhong <86729343+WenMinZhong@users.noreply.github.com>
2025-06-14 00:12:43 +0800

    ESN.py

--- MNIST/ESN.py
+++ MNIST/ESN.py
@@ -18,22 +18,18 @@ class ESN():
         self.W = (torch.randint(size=(resSize, resSize), high=4, low=0,device=device))*1.0 # Reservoir --> Reservoir
         self.W[torch.randn(resSize, resSize,device=device) > self.sparsity] = 0
 
-
-
-
     def reservoir(self, data, new_start=True):
         self.dm = torch.zeros((data.shape[0], 1+self.resSize+self.inSize),device=device)
         if new_start:#
             self.R = (torch.randint(low=0,high=3,size=(1, self.resSize),device=device)-1)*0
-
-
         for t in range(data.shape[0]):
             self.R.to(device)
             u = torch.tensor(data[t], dtype=torch.float64,device=device)# initialize input at timestep t
             self.R = (1 - self.alpha)*self.R.cuda() +                              \
                 self.alpha*ptanh(torch.matmul(torch.hstack((torch.tensor([1],dtype=torch.float64).to(device), u)), self.Win.to(device)) + \
                                    reservoir.poperation(self.R, self.W))
-           self.dm[t] = torch.cat((torch.cat((torch.tensor([1],dtype=torch.float64,device=device),u), dim=0), self.R.flatten()),dim=0)
+            self.dm[t] = torch.cat((torch.cat((torch.tensor([1],dtype=torch.float64,device=device),u), dim=0), self.R.flatten()),dim=0)
 
         return self.dm
+
 
